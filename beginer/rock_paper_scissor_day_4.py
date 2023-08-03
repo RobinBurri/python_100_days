8e3c04aebe90dce66c96a974a894a7bbbe59ee0d
Robin Burri <robin@Robins-MacBook-Air.local>
2023-08-03 16:09:44 +0200

    day4 rock_paper_scissor done

---
+++ beginer/rock_paper_scissor_day_4.py
@@ -0,0 +1,55 @@
+# Rock Paper Scissors Game
+import random
+from asci_art import rock, paper, scissors
+from enum import Enum
+
+choice = ["rock", "paper", "scissors"]
+
+
+print("What do you choose?\nType 0 for Rock\nType 1 for Paper\nType 2 for Scissors")
+user_choice = int(input())
+computer_choice = random.randint(0, 2)
+game_result = ""
+user_asci = ""
+computer_asci = ""
+
+
+if user_choice == 0:
+    user_asci = rock
+    if computer_choice == 0:
+        computer_asci = rock
+        game_result = "draw"
+    elif computer_choice == 1:
+        computer_asci = paper
+        game_result = "lose"
+    elif computer_choice == 2:
+        computer_asci = scissors
+        game_result = "win"
+elif user_choice == 1:
+    user_asci = paper
+    if computer_choice == 0:
+        computer_asci = rock
+        game_result = "win"
+    elif computer_choice == 1:
+        computer_asci = paper
+        game_result = "draw"
+    elif computer_choice == 2:
+        computer_asci = scissors
+        game_result = "lose"
+elif user_choice == 2:
+    user_asci = scissors
+    if computer_choice == 0:
+        computer_asci = rock
+        game_result = "lose"
+    elif computer_choice == 1:
+        computer_asci = paper
+        game_result = "win"
+    elif computer_choice == 2:
+        computer_asci = scissors
+        game_result = "draw"
+
+x = choice[computer_choice]
+print(f"Computer chose : " + computer_asci)
+y = choice[user_choice]
+print(f"You chose : " + user_asci)
+print(f"Result is:  you {game_result}")
