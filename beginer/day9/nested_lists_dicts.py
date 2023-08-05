1aec8d9fb50bbc4f8b030e1e59ef5daca1d2b69b
Robin Burri <robin@Robins-MacBook-Air.local>
2023-08-05 11:28:48 +0200

    nested list and dictionary

---
+++ beginer/day9/nested_lists_dicts.py
@@ -0,0 +1,36 @@
+# Nesting Dictionary in a Dictionary
+
+travel_log = {
+    "France": {"cities_visited": ["Paris", "Lille", "Dijon"], "total_visits": 12},
+    "Germany": {
+        "cities_visited": ["Berlin", "Hamburg", "Stuttgart"],
+        "total_visits": 5,
+    },
+}
+
+# Nesting Dictionary in a List
+travel_log1 = [
+    {
+        "country": "France",
+        "cities_visited": ["Paris", "Lille", "Dijon"],
+        "total_visits": 12,
+    },
+    {
+        "country": "Germany",
+        "cities_visited": ["Berlin", "Hamburg", "Stuttgart"],
+        "total_visits": 5,
+    },
+]
+
+def add_new_country(country_visited: str, city_visited: list, total_visits: int):
+    travel_log1.append(
+        {
+            "country": country_visited,
+            "cities_visited": city_visited,
+            "total_visits": total_visits,
+        }
+    )
+
+add_new_country("Russia", ["Moscow", "Saint Petersburg"], 2)
+
+print(travel_log1)
