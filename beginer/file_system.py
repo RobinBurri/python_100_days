60e2a7707f27231125320be6d2ad0366e4db1a6b
Robin Burri <robin@Robins-MacBook-Air.local>
2023-08-06 15:19:56 +0200

    more file_system

--- beginer/file_system.py
+++ beginer/file_system.py
@@ -11,3 +11,19 @@ print("Current working directory:", os.getcwd())
 with open("text.txt", mode="r", encoding="utf-8") as file:
     content = file.read()
     print(content)
+
+
+# names = []
+# letter_content = ""
+
+# with open("../Names/names_list.txt", "r", encoding="utf-8") as file:
+#     lines = file.readlines()
+#     for name in lines:
+#         names.append(name.strip())
+
+# with open("../Letter/starting_letter.txt", "r", encoding="utf-8") as f:
+#     letter_content = f.read()
+
+# for name in names:
+#     with open(f"../readyToSend/letter_{name}.txt", "w", encoding="utf-8") as f:
+#         f.write(letter_content.replace("[name]", name))
