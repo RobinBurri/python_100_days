7a90c43965f905203b8bd34fb03cb496e9863035
Robin Burri <rburri@c1r16s5.42lausanne.ch>
2023-08-07 11:46:58 +0200

    tkinter label add

--- beginer/tkinter/main.py
+++ beginer/tkinter/main.py
@@ -2,10 +2,11 @@ import tkinter
 
 window = tkinter.Tk()
 window.title("Hello GUI World")
+window.minsize(width=500, height=300)
 
-
-
-
+#Label
+my_label = tkinter.Label(text="I'm a label", font=("Arial", 24))
+my_label.pack()
 
 
 
