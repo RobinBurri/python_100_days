e7c8b5e04bb30a5490410d8f34765b821e2cfe63
Robin Burri <robin@Robins-MacBook-Air.local>
2023-08-09 18:42:13 +0200

    tkinter wip

--- beginer/tkinter/main.py
+++ beginer/tkinter/main.py
@@ -4,21 +4,22 @@ window = tkinter.Tk()
 window.title("Hello GUI World")
 window.minsize(width=500, height=300)
 
-#Label
+# Label
 my_label = tkinter.Label(text="I'm a label", font=("Arial", 24))
 my_label.pack()
+my_label["text"] = "New Text"
 
+# Button
+def button_clicked():
+    my_label.config(text=ipt.get())
+    ipt.delete(0, tkinter.END)
 
+button = tkinter.Button(text="Click me", command=button_clicked)
+button.pack()
 
+# Entry
 
-
-
-
-
-
-
-
-
-
+ipt = tkinter.Entry(width=10)
+ipt.pack()
 
 window.mainloop()
