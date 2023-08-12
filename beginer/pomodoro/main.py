7050f8938872627174dfab2880842bb878096184
Robin Burri <robin@Robins-MacBook-Air.local>
2023-08-12 21:43:54 +0200

    pomodoro starting

---
+++ beginer/pomodoro/main.py
@@ -0,0 +1,27 @@
+import tkinter
+# ---------------------------- CONSTANTS ------------------------------- #
+PINK = "#e2979c"
+RED = "#e7305b"
+GREEN = "#9bdeac"
+YELLOW = "#f7f5dd"
+FONT_NAME = "Courier"
+WORK_MIN = 25
+SHORT_BREAK_MIN = 5
+LONG_BREAK_MIN = 20
+
+# ---------------------------- TIMER RESET ------------------------------- # 
+
+# ---------------------------- TIMER MECHANISM ------------------------------- # 
+
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+
+# ---------------------------- UI SETUP ------------------------------- #
+window = tkinter.Tk()
+window.title("Pomodoro")
+tomato_image = tkinter.PhotoImage(file="tomato.png")
+
+canvas = tkinter.Canvas(width=200, height=224)
+canvas.create_image(100, 112, image=tomato_image)
+
+
+window.mainloop()
