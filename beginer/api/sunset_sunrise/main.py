98c4b620dcbbd83c4aaa118a867f91650de8ec8b
Robin Burri <robin@Robins-MacBook-Air.local>
2023-08-18 23:01:47 +0200

    d33 done

---
+++ beginer/api/sunset_sunrise/main.py
@@ -0,0 +1,21 @@
+import requests
+
+lausanne_lat = 46.520
+lausanne_long = 6.620
+
+API_URL = "https://api.sunrise-sunset.org/json"
+
+parameters = {
+    "lat": lausanne_lat,
+    "lng": lausanne_long,
+    "formatted": 0
+}
+
+
+response = requests.get(url=API_URL, params=parameters)
+response.raise_for_status()
+data = response.json()
+sunrise =  data["results"]["sunrise"]
+sunset = data["results"]["sunset"]
+
+print(f"Sunrise: {sunrise.split('T')[1].split('+')[0]} / Sunset: {sunset.split('T')[1].split('+')[0]}") 
