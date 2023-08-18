d82446a94812c64de32629b27c7d20e42f226477
Robin Burri <robin@Robins-MacBook-Air.local>
2023-08-18 10:04:47 +0200

    exception handling

--- beginer/exceptions.py
+++ beginer/exceptions.py
@@ -1,2 +1,17 @@
-with open("a_file.txt", "r", encoding="utf-8") as f:
-    print(f.read())
+# FileNotFoundError (the file doesn't exist)
+
+try:
+    file = open("file.txt", encoding="utf-8")
+    a_dictionary = {"key": "value"}
+    print(a_dictionary["not_exist"])
+except FileNotFoundError:
+    file = open("file.txt", "w", encoding="utf-8")
+    file.write("Hello World")
+except KeyError as error_message:
+    print(f"KeyError: {error_message}")
+
+else:
+    content = file.read()
+    print(content)
+finally:
+    file.close()  # type: ignore
--- beginer/pandas/main.py
+++ beginer/pandas/main.py
@@ -1,33 +1,37 @@
-student_dict = {
-    "student": ["Angela", "James", "Lily"], 
-    "score": [56, 76, 98]
-}
+student_dict = {"student": ["Angela", "James", "Lily"], "score": [56, 76, 98]}
 
-#Looping through dictionaries:
-for (key, value) in student_dict.items():
-    #Access key and value
+# Looping through dictionaries:
+for key, value in student_dict.items():
+    # Access key and value
     pass
 
 import pandas
+
 student_data_frame = pandas.DataFrame(student_dict)
 
-#Loop through rows of a data frame
-for (index, row) in student_data_frame.iterrows():
-    #Access index and row
-    #Access row.student or row.score
+# Loop through rows of a data frame
+for index, row in student_data_frame.iterrows():
+    # Access index and row
+    # Access row.student or row.score
     pass
 
 # Keyword Method with iterrows()
 # {new_key:new_value for (index, row) in df.iterrows()}
 
-#TODO 1. Create a dictionary in this format:
 data = pandas.read_csv("nato_phonetic_alphabet.csv")
 nato_dict = {row.letter: row.code for (index, row) in data.iterrows()}
 # print(nato_dict)
 
-#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
 
-word = input("Eneter a word: ").upper()
-new_word = [nato_dict[letter] for letter in word]
-print(new_word)
+def generate_phonetic():
+    word = input("Eneter a word: ").upper()
+    try:
+        new_word = [nato_dict[letter] for letter in word]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+        generate_phonetic()
+    else:
+        print(new_word)
 
+
+generate_phonetic()
