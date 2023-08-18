466933526a0a99de9e1f6e743d6fb5d6353fe404
Robin Burri <robin@Robins-MacBook-Air.local>
2023-08-18 22:30:17 +0200

    kanye api done

--- beginer/api/space_station.py/main.py
+++ beginer/api/space_station.py/main.py
@@ -1,3 +1,14 @@
 import requests
 
 API_URL = "http://api.open-notify.org/iss-now.json"
+
+response = requests.get(url=API_URL)
+
+response.raise_for_status()
+
+data = response.json()
+
+longitude = data["iss_position"]["longitude"]
+latitude = data["iss_position"]["latitude"]
+
+iss_position = (longitude, latitude)
