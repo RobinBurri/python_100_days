12530cd61d497a22e38d0fab63cee234070fb5f5
Robin Burri <robinburri@tutanota.com>
2023-08-23 22:50:57 +0200

    wip

--- beginer/soup/beginer/main.py
+++ beginer/soup/beginer/main.py
@@ -21,3 +21,12 @@ soup = bs4.BeautifulSoup(content, "html.parser")
 all_anchor_tag = soup.find_all(name="a")
 for tag in all_anchor_tag:
     print(bcolors.OKCYAN + tag.get("href") + bcolors.ENDC)
+
+# other exemples
+heading = soup.find(name="h1", id="name")
+print(heading)
+section_heading = soup.find(name="h3", class_="heading")
+print(section_heading.getText())
+
+name = soup.select_one(selector="#name")
+print(name)
